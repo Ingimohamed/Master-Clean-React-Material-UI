0a2d2f6d5a126d88a678771306c8c78c7b9ee8c0
Ingi Mohamed <ingimohamed2222@gmail.com>
2021-02-19 11:47:21 +0200

    updates

--- master-clean/src/components/contOrderReview.jsx
+++ master-clean/src/components/contOrderReview.jsx
@@ -6,20 +6,41 @@ import MuiAccordionDetails from '@material-ui/core/AccordionDetails';
 import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
 import Divider from '@material-ui/core/Divider';
+import ordertshirt from '../images/order-tshirt.png';
+import orderpullover from '../images/order-pullover.png';
 
 
 
 
 const useStyles = makeStyles((theme) => ({
-  accordion: {
+    accordion: {
     margin: "15px 0",
     borderRadius: "20px",
     boxShadow:"0 2px 5px 0 rgba(0, 0, 0, 0.16), 0 2px 10px 0 rgba(0, 0, 0, 0.12) !important",
   },
   title: {
     fontWeight:"700",
-  },
-  AccorBody:{},
+    },
+    AccordionBody: {
+        padding: "30px!important",
+    },
+    p_title: {
+        fontWeight: "700",
+        fontSize: "17px",
+        color: "#00527D",
+        fontFamily:"'Poppins', sans-serif",
+    },
+    p: {
+        fontWeight: "400",
+        fontSize: "17px",
+        color: "#00527D",
+        fontFamily:"'Poppins', sans-serif",
+    },
+    delivery: {
+        color: "#EFB6B6",
+        fontSize: "17px",
+        fontFamily:"'Poppins', sans-serif",
+    },
 }));
 const Accordion = withStyles({
   root: {
@@ -85,43 +106,53 @@ export default function ContOrderReview() {
         <AccordionSummary aria-controls="panel1d-content" id="panel1d-header">
           <Typography  className={classes.title}>REVIEW YOUR ORDER</Typography>
         </AccordionSummary>
-        <AccordionDetails>
+        <AccordionDetails className={classes.AccordionBody}>
           <Grid container className="mb-1">
             <Grid item md={6}> 
-              image
+            <img className="reviewImg" src={ordertshirt} alt="tshirt"/>
             </Grid>
             <Grid item md={6}>
-              tshirt
+                <Typography className={classes.p_title} variant="body2" component="p">T-shirt</Typography>
+                 <Typography className={classes.p} variant="body2" component="p">D(dry-clean)</Typography>
+                <Typography className={classes.p} variant="body2" component="p">Price: 20 EGP</Typography>
+                <Typography className={classes.p} variant="body2" component="p">Quantity: 1</Typography>
             </Grid>
           </Grid>
-          <Grid container className="mb-1">
+          <Grid container className="mb-4">
             <Grid item md={6}> 
-              image
+            <img className="reviewImg" src={orderpullover} alt="pullover"/>
             </Grid>
             <Grid item md={6}>
-               pullover
+                <Typography className={classes.p_title} variant="body2" component="p">Pullover</Typography>
+                 <Typography className={classes.p} variant="body2" component="p">D(dry-clean)</Typography>
+                <Typography className={classes.p} variant="body2" component="p">Price: 30 EGP</Typography>
+                <Typography className={classes.p} variant="body2" component="p">Quantity: 1</Typography>
             </Grid>
      </Grid>
-    <Grid container className="mb-1">
+    <Grid container>
         <Grid item md={6}>
-            subtotal
+         <Typography className={classes.p}>subtotal</Typography>   
         </Grid>
         <Grid item md={6}>
-           50 EGP
+          <Typography className={classes.p}>50 EGP</Typography> 
         </Grid>            
         </Grid>
-    <Grid container className="mb-1">
+    <Grid container>
         <Grid item md={6}>
-            subtotal
+           <Typography className={classes.delivery} variant="body2" component="p">Delivery</Typography> 
         </Grid>
         <Grid item md={6}>
-           50 EGP
+          <Typography className={classes.delivery} variant="body2" component="p">10 EGP</Typography> 
         </Grid>            
     </Grid>
-    <Divider />
+    <Divider className="divider" />
         <Grid container>
-           <Grid item md={6}>TOTAL</Grid>
-           <Grid item md={6}>60 EGP</Grid>
+           <Grid item md={6}>
+               <Typography className={classes.p}>TOTAL</Typography>
+            </Grid>
+           <Grid item md={6}>
+               <Typography className={classes.p}>60 EGP</Typography>
+            </Grid>
         </Grid>          
         </AccordionDetails>
       </Accordion>
